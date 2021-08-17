8889712102882111e93f1546e3ad3ec68c7ae36e
William Crews <crewswh@gmail.com>
2021-08-17 13:15:04 -0400

    yup again 2021

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = require('./db')
+const db = require('./models')
+const config = require('./config/routes')
 
 const app = express()
 const apiPort = 3333
@@ -16,5 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', config)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
