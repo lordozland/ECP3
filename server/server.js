c8ee2601c7eb31f734856ac7f25f9b0f955d7862
William Crews <crewswh@gmail.com>
2021-08-18 13:16:01 -0400

    yay!

--- server/server.js
+++ server/server.js
@@ -7,17 +7,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-window.onload = (event) => {
-    event.preventDefault()
-    var hello = document.createElement('h1')
-    var create = document.createTextNode("Hello")
-    hello.appendChild(create)
-    document.appendChild(hello)
-    console.log('page is fully loaded');
-  };
-
-
-
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
 
