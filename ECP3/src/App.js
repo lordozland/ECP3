2d78c0941fdd02199cc8e6904d7badc0b24ff8e0
William Crews <crewswh@gmail.com>
2021-08-22 15:26:48 -0400

    wrong

---
+++ ECP3/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'materialize-css';
+// import { Button, Card, Row, Col, Container } from 'react-materialize';
+
+function App() {
+  return (
+    <><iframe title='muzak' width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1052206234&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-234357210" title="Ptocojin Mystikoz" target="_blank" style="color: #cccccc; text-decoration: none;">Ptocojin Mystikoz</a> · <a href="https://soundcloud.com/user-234357210/something" title="Something Beautiful_DEMO_19May21.wav" target="_blank" style="color: #cccccc; text-decoration: none;">Something Beautiful_DEMO_19May21.wav</a></div>
+
+    <div class="container">
+      <div class="row">
+
+      <h3 class="center">Rebuilding in React</h3>
+
+      <p>Recently broke a guitar.  Album is currently complete as it will remain until I get another 7 string.  Writing new material after I finish my coding bootcamp in three/four weeks.  Thank you for your patience.</p>
+
+      </div>
+    </div></>
+  );
+}
+
+export default App;
