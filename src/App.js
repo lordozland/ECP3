a713cafdd7953b2b9f1b8e965038917227a968f7
William Crews <crewswh@gmail.com>
2021-08-24 15:47:24 -0400

    just like handling them bars!

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import './App.css';
 import 'materialize-css';
 import Navbar from './components/layout/Navbar';
 import Tracks from './components/layout/Tracks';
+import MainPage from './pages/MainPage'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Tracks />
-        <div class="container">
-            <div class="row">
-
-            <h3 class="center">Rebuilding in React</h3>
-
-            <p>Recently broke a guitar.  Album is currently complete as it will remain until I get another 7 string.  Writing new material after I finish my coding bootcamp in three/four weeks.  Thank you for your patience.</p>
-
-            </div>
-        </div>
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div class="container">
+            <div class="row">
+
+            <h3 class="center">Rebuilding in React</h3>
+
+            <p>Recently broke a guitar.  Album is currently complete as it will remain until I get another 7 string.  Writing new material after I finish my coding bootcamp in three/four weeks.  Thank you for your patience.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default MainPage
