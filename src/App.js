7f17d04038ee0376fe9f1e53ee822c3f0d21a725
William Crews <crewswh@gmail.com>
2021-08-24 13:19:18 -0400

    back

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import 'materialize-css';
-// import { Row, Col, Container } from 'react-materialize';
-import Navbar from './components/layouts/navbar'
-
 
 function App() {
   return (
     <div>
-      <Navbar />
-    </div> 
+      <h1 className="center">Here</h1>
+    </div>
   );
 }
 
