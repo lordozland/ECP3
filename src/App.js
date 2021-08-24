0f39dc89cc4e3ad7b4c77e016fc6ba2d07b7131d
William Crews <crewswh@gmail.com>
2021-08-24 14:32:16 -0400

    got it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import 'materialize-css';
 import { Container, Row, Col } from 'react-materialize';
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Container>
         <Row>
-          <Col className="center"><h1>Here</h1></Col>
+          <Col class="center"><h1>Here</h1></Col>
         </Row>
       </Container>
     </div>
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+// Here Baby 2.0.1!!!
+
+import React from 'react';
+import { Component } from 'react';
+import 'materialize-css';
+
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav class="nav-extended black">
+                    <div class="nav-wrapper">
+                    <a href="/home" class="brand-logo center">Equinox Crest Productions</a>
+                    </div>
+                    <div class="nav-content">
+                    <ul class="tabs tabs-transparent center">
+                        <li class="tab"><a href="/life">Life</a></li>
+                        <li class="tab"><a href="/sound">Sound</a></li>
+                        <li class="tab"><a href="/design">Design</a></li>
+                        <li class="tab"><a href="/company">Company</a></li>
+                        <li class="tab"><a href="/contact">Contact</a></li>
+                    </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+};
+
+export default Navbar;
