331cc741f4a0ce6967b1a5200d18267fdf62a329
William Crews <crewswh@gmail.com>
2021-08-24 15:50:22 -0400

    I am so happy it physically hurts

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,9 +5,9 @@ const MainPage = () => {
         <div class="container">
             <div class="row">
 
-            <h3 class="center">Rebuilding in React</h3>
+            <h3 class="center">Rebuilding in React!!!!!!!!!!!</h3>
 
-            <p>Recently broke a guitar.  Album is currently complete as it will remain until I get another 7 string.  Writing new material after I finish my coding bootcamp in three/four weeks.  Thank you for your patience.</p>
+            <p>Recently broke a guitar.  Album is currently complete as it will remain until I get another 7 string.  Writing new material after I finish my coding bootcamp in three weeks.  Thank you for your patience.</p>
 
             </div>
         </div>
