8e3ca0d55eee50ccaff016d1978329bace6f2b91
William Crews <crewswh@gmail.com>
2021-08-24 16:19:45 -0400

    chomp

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import './App.css';
 import 'materialize-css';
 import Navbar from './components/layout/Navbar';
 import Tracks from './components/layout/Tracks';
-import MainPage from './pages/MainPage'
 
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Tracks />
-      <MainPage />
+      <Router>
+        <Route path="/" component={MainPage} />
+      </Router>
     </div>
   );
 }
+
+// class App extends Component {
+//   render() {
+//     return <Router>
+//       <Route path="/" component={MainPage} />
+//     </Router>
+//   }
+// }
 
 export default App;
