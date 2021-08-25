2c08012f6c600bf02c9c00cb73022cceae562930
William Crews <crewswh@gmail.com>
2021-08-25 11:44:21 -0400

    words

--- src/pages/Life.jsx
+++ src/pages/Life.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Life = () => {
+    const style = {
+        fontweight: 'italic'
+    };
+
     return (
         <div>
             <div class="container center">
@@ -9,7 +13,7 @@ const Life = () => {
                         <h3>Life Is</h3>
                     </div>
                     <div class="col s12 m12 l12 center">
-                        <h4>Happening</h4>
+                        <h5>Happening.  I am in the final stretch of my Bootcamp.  All seems well, but KRUNCH time is upon us.</h5>
                     </div>
                 </div>
             </div>
--- src/pages/Sound.jsx
+++ src/pages/Sound.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from "react-player";
 
 const Sound = () => {
     return (
@@ -9,7 +10,29 @@ const Sound = () => {
 
             <p class="center">Mwahahahahahahaha!</p>
 
+            <div>
+            <ReactPlayer width="100%" height="200px" className="TrakzPage"
+            url="https://soundcloud.com/user-234357210/nova-demo-09jun21-wav"
+            />
             </div>
+            <div>
+            <ReactPlayer width="100%" height="200px" className="TrakzPage"
+            url="https://soundcloud.com/user-234357210/scamp-demo-22may21_2-wav"
+            />
+            </div>
+            </div>
+            <div>
+            <ReactPlayer width="100%" height="200px" className="TrakzPage"
+            url="https://soundcloud.com/user-234357210/something"
+            />
+            </div>
+            <div>
+            <ReactPlayer width="100%" height="200px" className="TrakzPage"
+            url="https://soundcloud.com/user-234357210/hyperemesis-gravidarum-demo"
+            />
+            </div>
+
+            
         </div>
     )
 }
